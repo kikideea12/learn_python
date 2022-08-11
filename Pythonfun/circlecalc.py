f0b61a6cf1fe7a4eef9fb711458df0b9e44e5f45
Karissa Oniciuc <karissa.deea@gmail.com>
2022-08-11 14:53:19 -0700

    new file

---
+++ Pythonfun/circlecalc.py
@@ -0,0 +1,16 @@
+
+from tkinter import Y
+
+
+x = "Do you want to find the area(1), diameter(2), or circumference(3) of a circle: "
+y = "What is the radius of your circle"
+
+print(input(x))
+
+if x == 1:
+    print(y)
+
+
+
+
+
