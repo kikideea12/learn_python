305b8a89777e7a53dfc0aa4896a8aa5eba6e88f7
Karissa Oniciuc <karissa.deea@gmail.com>
2022-08-12 16:20:39 -0700

    more lessons

---
+++ LearnNumpy/creatingarrays.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print(type(arr))
+
+arr = np.array((1, 2, 3, 4, 5))
+
+print(arr)
+
+# Nested array: are arrays that have arrays as their elements.
+
+#0-D arrays
+
+arr = np.array(42)
+
+print(arr)
+
+#1-D arrays
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+#2-D arrays
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(arr)
+
+#3-D arrays
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(arr)
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
+
+arr = np.array([1, 2, 3, 4], ndmin=5)
+
+print(arr)
+print('number of dimensions :', arr.ndim)
+
+
+
+
+
+
---
+++ LearnNumpy/gettingstarted.py
@@ -0,0 +1,15 @@
+import numpy
+
+arr = numpy.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+# alias: In Python alias are an alternate name for referring to the same thing.
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print(np.__version__)
