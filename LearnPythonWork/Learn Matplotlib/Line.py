f75cb8c6106b3e71955877b907b9c73d13bb7783
Karissa Oniciuc <karissa.deea@gmail.com>
2022-08-15 18:36:04 -0700

    Matplotlib intro lessons

---
+++ LearnPythonWork/Learn Matplotlib/Line.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linestyle = 'dotted')
+plt.show()
+
+plt.plot(ypoints, color = 'r')
+plt.show()
+
+plt.plot(ypoints, color = '#4CAF50')
+plt.show()
+
+plt.plot(ypoints, linewidth = '20.5')
+plt.show()
+
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(y1)
+plt.plot(y2)
+
+plt.show()
+
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(x1, y1, x2, y2)
+plt.show()
+
+
+
+
+
---
+++ LearnPythonWork/Learn Matplotlib/Subplot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
